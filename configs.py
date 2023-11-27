ffe1934effd25322afe089b332d4f73acf4c2776
Rohit Doriya <95335223+FreakyAI-star@users.noreply.github.com>
2023-11-28 03:33:47 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,6 +1,6 @@
-save_dir                    = '/work/newriver/wyharveychen/CloserLookFewShot/'
+save_dir                    = '/content/CloserLookFewShot'
 data_dir = {}
-data_dir['CUB']             = './filelists/CUB/' 
-data_dir['miniImagenet']    = './filelists/miniImagenet/' 
-data_dir['omniglot']        = './filelists/omniglot/' 
-data_dir['emnist']          = './filelists/emnist/' 
+data_dir['CUB']             = '/content/CloserLookFewShot/filelists/CUB/' 
+data_dir['miniImagenet']    = '/content/CloserLookFewShot/filelists/miniImagenet/' 
+data_dir['omniglot']        = '/content/CloserLookFewShot/filelists/omniglot/' 
+data_dir['emnist']          = '/content/CloserLookFewShot/filelists/emnist/' 
